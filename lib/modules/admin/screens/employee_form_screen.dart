5b2edf91885a89455410fc0b6068b1c62efe163c
MannDangrechiya <147187055+MannDangrechiya@users.noreply.github.com>
2026-02-24 11:01:10 +0530

    dashboard

--- lib/modules/admin/screens/employee_form_screen.dart
+++ lib/modules/admin/screens/employee_form_screen.dart
@@ -235,7 +235,7 @@ class _EmployeeFormScreenState extends State<EmployeeFormScreen> {
               ),
               const SizedBox(height: 16),
               DropdownButtonFormField<String>(
-                value: _status,
+                initialValue: _status,
                 decoration: const InputDecoration(
                   labelText: 'Status',
                   border: OutlineInputBorder(),
--- lib/modules/super_admin/super_admin_dashboard.dart
+++ lib/modules/super_admin/super_admin_dashboard.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import '../authentication/auth_controller.dart';
 
 class SuperAdminDashboard extends StatelessWidget {
